00f429b2681f4b90c96c76ba638520571dd94f42
Rishabh Jain <107530140+rishabhjain208@users.noreply.github.com>
2026-01-31 21:00:09 +0530

    update api route

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://order-management-backend-if9a.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
